e95076d5ca4fe6c4cc120f82bc90de3a50574ad7
Waren Gonzaga <github@warengonzaga.com>
2021-03-08 04:15:55 +0800

    👌 IMPROVE: Add new DNS servers

--- src/core.bat
+++ src/core.bat
@@ -16,29 +16,29 @@ echo Administrative permissions required. Detecting permissions...
 ping localhost -n 2 >NUL
 net session >nul 2>&1
 if %errorLevel% == 0 (
-echo Administrator privileges found!
-echo Starting the program...
-ping localhost -n 2 >NUL
-goto mainMenu
+    echo Administrator privileges found!
+    echo Starting the program...
+    ping localhost -n 2 >NUL
+    goto mainMenu
 ) else (
-cls
-color %erruicolor%
-echo # %divider%
-echo # %appname% v%appvers% - %appstat%
-echo # by %dev%
-echo # %divider%
-echo #
-echo # ERROR * ERROR * ERROR * ERROR * ERROR * ERROR
-echo #
-echo # Current user permissions to execute this .bat file are inadequate.
-echo # This .bat file must be run with administrative privileges.
-echo # Close this program and run it as administrator.
-echo # Contact the developer to assist you...
-echo #
-echo # ERROR * ERROR * ERROR * ERROR * ERROR * ERROR
-echo #
-pause
-exit
+    cls
+    color %erruicolor%
+    echo # %divider%
+    echo # %appname% v%appvers% - %appstat%
+    echo # by %dev%
+    echo # %divider%
+    echo #
+    echo # ERROR * ERROR * ERROR * ERROR * ERROR * ERROR
+    echo #
+    echo # Current user permissions to execute this .bat file are inadequate.
+    echo # This .bat file must be run with administrative privileges.
+    echo # Close this program and run it as administrator.
+    echo # Contact the developer to assist you...
+    echo #
+    echo # ERROR * ERROR * ERROR * ERROR * ERROR * ERROR
+    echo #
+    pause
+    exit
 )
 
 rem =============================
@@ -107,14 +107,69 @@ echo # Default / DHCP ............. [0]
 echo # Google DNS ................. [1]
 echo # Cloudflare DNS ............. [2]
 echo # Freenome DNS ............... [3]
-echo # Back ....................... [4] (enter)
+echo # Comodo DNS ................. [4]
+echo # Quad9 DNS .................. [5]
+echo # Verisign DNS ............... [6]
+echo # OpenDNS .................... [7]
+echo # Back ....................... [8] (enter)
 echo #
 set/p "t001_option=# %cliname%> " || set t001_option=4
-if %t001_option%==0 set t001_d0=1 & goto t001_1
-if %t001_option%==1 set t001_d0=0 & set t001_d1=8.8.8.8 & set t001_d2=8.8.4.4 & goto t001_1
-if %t001_option%==2 set t001_d0=0 & set t001_d1=1.1.1.1 & set t001_d2=1.0.0.1 & goto t001_1
-if %t001_option%==3 set t001_d0=0 & set t001_d1=80.80.80.80 & set t001_d2=80.80.81.81 & goto t001_1
-if %t001_option%==4 goto toolsMenu
+rem some algo
+if %t001_option% equ 0 (
+    rem default / dhcp
+    set t001_d0=1
+    goto t001_1
+)
+if %t001_option% equ 1 (
+    rem Google Public DNS
+    set t001_d0=0
+    set t001_d1=8.8.8.8
+    set t001_d2=8.8.4.4
+    goto t001_1
+)
+if %t001_option% equ 2 (
+    rem Cloudflare DNS
+    set t001_d0=0
+    set t001_d1=1.1.1.1
+    set t001_d2=1.0.0.1
+    goto t001_1
+)
+if %t001_option% equ 7 (
+    rem Freenom DNS
+    set t001_d0=0
+    set t001_d1=80.80.80.80
+    set t001_d2=80.80.81.81
+    goto t001_1
+)
+if %t001_option% equ 4 (
+    rem Comodo Secure DNS
+    set t001_d0=0
+    set t001_d1=8.26.56.26
+    set t001_d2=8.20.247.20
+    goto t001_1
+)
+if %t001_option% equ 5 (
+    rem Quad9 DNS
+    set t001_d0=0
+    set t001_d1=9.9.9.9
+    set t001_d2=149.112.112.112
+    goto t001_1
+)
+if %t001_option% equ 6 (
+    rem Verisign DNS
+    set t001_d0=0
+    set t001_d1=64.6.64.6
+    set t001_d2=64.6.65.6
+    goto t001_1
+)
+if %t001_option% equ 7 (
+    rem OpenDNS
+    set t001_d0=0
+    set t001_d1=208.67.222.222
+    set t001_d2=208.67.220.220
+    goto t001_1
+)
+if %t001_option% equ 8 goto toolsMenu
 goto err2
 pause>null
 
