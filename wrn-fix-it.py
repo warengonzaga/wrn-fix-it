0a8839f0f279309c7a2597bb8327d5c93474c77c
angelofallars <ba.fallaria@gmail.com>
2021-08-31 23:07:50 +0800

    Add main menu and define the menu functions
    	modified:   wrn-fix-it.py

--- wrn-fix-it.py
+++ wrn-fix-it.py
@@ -1,4 +1,5 @@
 from os import system, name
+import sys
 
 appname = "WRN Fix IT"
 appvers = "1.0.0-rc.2"
@@ -7,8 +8,46 @@ dev = "Waren Gonzaga"
 desc = "Your companion toolset for fixing common issues"
 divider = "======================================"
 
+def tools_menu():
+    print("TODO: Tools menu")
+
+def modules_menu():
+    print("TODO: Modules menu")
+
+def donate():
+    print("TODO: Donate menu")
+
 def main_menu():
-    pass
+    print(f"# {divider}")
+    print(f"# {appname} v{appvers} - {appstat}")
+    print(f"# by {dev}")
+    print(f"# {divider}")
+    print(f"# {desc}")
+    print(f"# {divider}")
+    print("#")
+    print("# Tools ...................... [1] (enter)")
+    print("# Modules .................... [2] (coming soon)")
+    print("# Donate ..................... [3]")
+    print("# Exit ....................... [4]")
+    print("#")
+
+    while True:
+        user_input = input("mainMenu=# > ")
+    
+        if user_input == "1":
+            tools_menu()
+            break
+        elif user_input == "2":
+            modules_menu()
+            break
+        elif user_input == "3":
+            donate()
+            break
+        elif user_input == "4":
+            sys.exit()
+        else:
+            print("Invalid input. Try again.")
+            continue
 
 def main():
     main_menu()
