bb2a8fc7a5e3984934de4e5862d349a08231ff46
angelofallars <ba.fallaria@gmail.com>
2021-08-31 22:57:59 +0800

    Create wrn-fix-it.py

---
+++ wrn-fix-it.py
@@ -0,0 +1,17 @@
+from os import system, name
+
+appname = "WRN Fix IT"
+appvers = "1.0.0-rc.2"
+appstat = "Release Candidate"
+dev = "Waren Gonzaga"
+desc = "Your companion toolset for fixing common issues"
+divider = "======================================"
+
+def main_menu():
+    pass
+
+def main():
+    main_menu()
+
+if __name__ == '__main__':
+    main()
